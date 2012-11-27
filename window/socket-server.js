1af21b83ec4dd0d7094ca9599b697edb2d342d61
Andy Pruett <andy.pruett@gatech.edu>
2012-11-27 13:02:23 -0500

    60% through refactoring for straightup HTML version

--- window/socket-server.js
+++ window/socket-server.js
@@ -10,19 +10,19 @@ function handler (req, res) {
   var jsonpatt = new RegExp ('json$');
   var imgpatt = new RegExp ('png$');
   var csspatt = new RegExp ('css$');
+  var windowpatt = new RegExp ('^.*window.*html$');
 
-  if (req.url=='/window.html'){
-	  fs.readFile(__dirname + '/window.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
+  if (windowpatt.test(req.url)){
+	fs.readFile(__dirname + req.url, function (err, data) {
+		if (err) {
+			res.writeHead(500);
+			return res.end('Error loading index.html');
+		}
+		res.writeHead(200);
+		res.end(data);
+	});
   }
+  
   else if (req.url=='/clock.png'){
 	  fs.readFile(__dirname + '/clock.png',
   function (err, data) {
